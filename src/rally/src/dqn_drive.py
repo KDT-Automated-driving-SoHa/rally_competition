94791e157e554fca33328aa05f6fb6b120695b3c
sinabeulo90 <smbyun0214@gmail.com>
2021-03-04 09:49:03 +0900

    DQN 셋업

--- src/rally/src/dqn_drive.py
+++ src/rally/src/dqn_drive.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python
+import numpy as np
 
-from model import *
+from dqn.dqn import DQN
 
-class DQN:
+class DQN_Drive:
     angle = 0
     speed = 0
     
-    def __init__(self, hidden_size, pth_path):
-        study_init(5, hidden_size, "DQN")
-        self.load_pth(pth_path)
+    def __init__(self, input_size, output_size, hidden_size, model_episode_no, skip_frame=2, stack_frame=5):
+        self.dqn = DQN(input_size, output_size, hidden_size, stack_frame=stack_frame, skip_frame=skip_frame)
+        self.dqn.model_load(model_episode_no)
 
-    def load_pth(path):
-        study_model_load(path)
 
     def set_motor(self, angle, speed):
         self.angle = angle
         self.motor = motor
 
+
     def set_data(self, data):
-        pass
+        data = np.array(data[:5])
+        while len(self.dqn.observation_set) < self.dqn.skip_frame * self.dqn.stack_frame:
+            self.dqn.observation_set.append(data)
+        
+        state = net.skip_stack_frame(data)  # 상태 생성
+        action = net.get_action(state, 0)   # 행동 선택
+
+        # 조향각 조정
+        if action == 0:
+            steering_deg = -30
+        elif action == 1:
+            steering_deg = 0
+        else:
+            steering_deg = 30
+
+        speed = 35
+
+        self.set_motor(steering_deg, speed)
+
 
     def get_motor(self):
         return self.angle, self.motor
