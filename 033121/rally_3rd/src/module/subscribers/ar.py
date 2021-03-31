a6ccba5a12b6f26c43563c6c45b2466f1767dd9b
sinabeulo90 <smbyun0214@gmail.com>
2021-03-31 10:03:15 +0900

    통합 완료, 테스트 예정

---
+++ 033121/rally_3rd/src/module/subscribers/ar.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+
+from ar_track_alvar_msgs.msg import AlvarMarkers
+from module.infos.ar_info import ARInfo
+
+
+class ARSubscriber():
+    def __init__(self, name="/ar_pose_marker"):
+        # 원본 라이다 데이터
+        self.raw_sign = None        # 주차 신호 정보
+        self.raw_parking = None     # 주차 위치 정보
+
+        # rospy subscriber
+        rospy.Subscriber(name, AlvarMarkers, self._callback, queue_size=1)
+
+
+    def _callback(self, msg):
+        self.raw_sign = None
+        self.raw_parking = None
+
+        for marker in markers:
+            if marker.id == 1:
+                self.raw_sign = marker
+            elif marker.id == 2:
+                self.raw_parking = marker
+
+
+    def get_parking_sign(self):
+        if self.raw_sign is None:
+            return False, None
+
+        position = self.raw_sign.pose.pose.position
+        orientation = self.raw_sign.marker.pose.pose.orientation
+        return True, ARInfo(position, orientation)
+
+
+    def get_parking_line(self):
+        if self.raw_parking is None:
+            return False, None
+
+        position = self.raw_parking.pose.pose.position
+        orientation = self.raw_parking.marker.pose.pose.orientation
+        return True, ARInfo(position, orientation)
---
+++ 033121/rally_3rd/src/module/subscribers/ultrasonic.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+
+import numpy as np
+
+from std_msgs.msg import Int32MultiArray
+from module.infos.ultrasonic_info import UltrasonicInfo
+
+
+class UltrasonicSubscriber():
+    def __init__(self, name="/xycar_ultrasonic"):
+        # 원본 초음파 데이터
+        self.raw_data = None
+
+        # rospy subscriber
+        rospy.Subscriber(name, Int32MultiArray, self._callback, queue_size=1)
+
+
+    def get(self):
+        if self.raw_data is None:
+            return False, None
+        return True, UltrasonicInfo(self.raw_data)
+            
+
+    def _callback(self, msg):
+        self.raw_data(msg.data)
